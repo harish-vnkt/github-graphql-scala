1099edc31cc0aed321e42f54a10194b02f0117fa
Harish238 <harishvenkat238@gmail.com>
2020-05-01 22:21:59 -0500

    Add QueryBuilder trait

---
+++ src/main/scala/builders/QueryBuilder.scala
@@ -0,0 +1,32 @@
+package builders
+
+trait QueryBuilder {
+
+  def scalars: List[String]
+  def fields: List[QueryBuilder]
+  def connections: List[QueryBuilder]
+
+  var topQuery: String
+
+  def construct(): String = {
+
+    var returnString: String = topQuery + " {"
+
+    for (scalar <- this.scalars) {
+      returnString = returnString + " " + scalar
+    }
+
+    for (field <- this.fields) {
+      returnString = returnString + " " + field.construct()
+    }
+
+    for (connection <- this.connections) {
+      connection.topQuery = connection.topQuery + " { edges { node"
+      returnString = returnString + " " + connection.construct()
+      returnString = returnString + " } }"
+    }
+
+    returnString + " }"
+  }
+
+}
