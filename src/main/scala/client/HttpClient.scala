e9c6da3cd5b34546117c8192b6231253fbbe7ea1
Karan Venkatesh Davanam <kdavan2@uic.edu>
2020-05-05 12:25:23 -0500

    added exception handling in client

--- src/main/scala/client/HttpClient.scala
+++ src/main/scala/client/HttpClient.scala
@@ -6,9 +6,11 @@ import com.google.gson.{Gson, JsonObject}
 import client.json.{GsonDeserializer, GsonJsonSerializer, JacksonJsonDeserializer}
 import models.objects.{GraphQLObject, Repository, Search, User}
 
+
+
 case class HttpClient(headerPart:Map[String,String]= Map()){
 
-  def executeQuery[T](queryObj:Query):T={
+  def executeQuery[T <: GraphQLObject](queryObj:Query):Option[T]={
     val jsonQuery:String = queryObj.getQueryString
     val returnType =  queryObj.getReturnType
     val jsonObj = jsonObjectCreation(jsonQuery)
@@ -17,17 +19,23 @@ case class HttpClient(headerPart:Map[String,String]= Map()){
       .headers(
         headerPart
       ).postData(jsonAst)
+    println(response.asString.body)
     val responseJson = GsonDeserializer.build.deserialize(response.asString.body)
-    val data = responseJson.getAsJsonObject("data").getAsJsonObject(returnType)
-    val jsonString = GsonJsonSerializer.build.serialize(data)
-    returnType match {
-      case "repository" => JacksonJsonDeserializer.build.deserialize[Repository](jsonString).asInstanceOf[T]
-      case "user" => JacksonJsonDeserializer.build.deserialize[User](jsonString).asInstanceOf[T]
-      case "search" => JacksonJsonDeserializer.build.deserialize[Search](jsonString).asInstanceOf[T]
+    if(!responseJson.has("errors")) {
+      val data = responseJson.getAsJsonObject("data").getAsJsonObject(returnType)
+      val jsonString = GsonJsonSerializer.build.serialize(data)
+      returnType match {
+        case "repository" => Some(JacksonJsonDeserializer.build.deserialize[Repository](jsonString).asInstanceOf[T])
+        case "user" => Some(JacksonJsonDeserializer.build.deserialize[User](jsonString).asInstanceOf[T])
+        case "search" => Some(JacksonJsonDeserializer.build.deserialize[Search](jsonString).asInstanceOf[T])
+      }
     }
-
-
+    else{
+      println("Build Query in the proper Format")
+      None
+    }
   }
+
 
   private def jsonObjectCreation(jsonString:String)={
     val jsonObj: JsonObject = new JsonObject()
