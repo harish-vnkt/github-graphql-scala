d5212d247fb4f05400514e4dc11fd2144bea86e1
Harish238 <harishvenkat238@gmail.com>
2020-05-05 11:13:00 -0500

    Add PageInfo field to Search class

--- src/main/scala/models/objects/Search.scala
+++ src/main/scala/models/objects/Search.scala
@@ -1,3 +1,7 @@
 package models.objects
 
-case class Search(nodes: List[Repository]) extends GraphQLObject
+case class Search(nodes: List[Repository], pageInfo: PageInfo) extends GraphQLObject {
+
+  def getPageInfo: PageInfo = pageInfo
+
+}
